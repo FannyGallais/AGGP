8451a36738f84a2859ac61b5596829d5c8c6ca6b
Gallais Fanny <fgallais@bs406-s31-14.insa-lyon.fr>
2016-03-30 10:13:52 +0200

    Retour a la bonne version

--- aggpGraphe.py
+++ aggpGraphe.py
@@ -107,31 +107,6 @@ def drawSCE(g):
 
 popu = Population.Population(50,0.1,0.8,100)
 
-<<<<<<< HEAD
-                
-"""
-              
-g=Graphe(10,0.6)
-
-
-print (g.graphe)
-print ("sceDegree:",g.sceDegrees())
-print ("sceCk:",g.sceCk())
-g.isConnexe()
-print ("calculCout" , g.calculCout())
-
-print "la matrice des plus courts chemins est",g.calcShortestPath()
-print g.SCESP()
-
-
-it = range(1,12)
-plt.plot(it,g.sceDegrees()[1],marker='o',color='cyan')
-plt.plot(it,g.sceDegrees()[2],marker='v',color='purple')
-plt.title("Distribution theorique de la somme des carres des ecarts / Distibution observee")
-#plt.legend([p1,p2],["Theorique","Observee"])
-plt.show()
-=======
->>>>>>> refs/remotes/origin/master
 
 
 
@@ -144,279 +119,21 @@ plt.plot(x,result[0],color="red")
 plt.plot(x,result[1],color="blue")
 plt.show()
 
-<<<<<<< HEAD
-"""
-
-####################################################################################
-##                                                                                ## 
-##                            CLASS POPULATION                                    ##
-##                                                                                ## 
-####################################################################################
-
-class Population:
-        def __init__(self,taillePop,proba,tailleGraphe,c=0.4):
-                self.p=taillePop
-                self.proba= proba
-                self.population= []
-                self.Wr=[]
-                for i in xrange(self.p):
-					self.Wr.append(self.p*((c-1)/(c**self.p-1))*c**(self.p-i))
-				
-                self.tailleGraphe = tailleGraphe
-                for i in range(self.p):
-					#print i
-					self.population.append(Graphe(tailleGraphe,self.proba))
-					self.Wr[i]=self.Wr[i]/sum(self.Wr)
-                       
-                
-				
-				   
-
-
-        def mutation(self,matrice):
-
-            for j in range(self.tailleGraphe):
-                for k in range(j,self.tailleGraphe):
-                        pMutation = random.random()
-                        if pMutation > self.proba :
-                                if matrice[j,k] == 0:
-                                        matrice[j,k] = 1
-                                        matrice[k,j] = 1
-                                else :
-                                        matrice[j,k] = 0
-                                        matrice[k,j] = 0
-              
-
-
-        def selection(self,c=0.4):
-			Cost = []
-			bestCost=[]
-			dico={}
-			bestCostSelect=[]
-			for i in xrange(self.p):
-				self.population[i].calculCout()
-
-          
-
-			for i in xrange(self.p):
-				Cost.append(self.population[i].cout)
-				if self.population[i].cout in dico:
-					dico[self.population[i].cout].append(i)
-				else:
-					dico[self.population[i].cout]=[i]
-					
-			Cost=sorted(Cost)
-			
-			for k in xrange(len(Cost)):
-				indGraphe=dico[Cost[k]][0]
-				bestCost.append(self.population[indGraphe])
-				dico[Cost[k]].remove(indGraphe)
-			
-			
-		
-			rang=np.random.multinomial(self.p-1,self.Wr)
-			
-			bestCostSelect.append(bestCost[0])
-			for i in xrange(len(rang)):
-				j=rang[i]
-				while j!=0:
-					bestCostSelect.append(bestCost[i])
-					j-=1
-			
-				
-			return bestCostSelect
-			
-			
-			
-
-        def croisement(self,dupBestPop,nbCrois):
-			for k in xrange(nbCrois):
-
-				# CROSSING OVER
-
-				## Mise en place du croisement entre les differents individus 
-				# Quel graphe croise avec quel graphe ? 
-				l1 = random.randint(0,dupBestPop[0].N-1)
-				g1 = random.randint(0,len(dupBestPop)-1)
-				g2 = random.randint(0,len(dupBestPop)-1)
-
-				while g1 == g2 : 
-					g2 = random.randint(0,len(dupBestPop)-1)
-				#print "l1",l1
-				#print "Valeurs de g1/g2 \n",g1,g2
-				#Puis on choisit aleatoirement les positions qui vont etre croises :
-				pos1 = random.randint(0,dupBestPop[0].N-1)
-				pos2 = random.randint(0,dupBestPop[0].N-1)
-				while pos1 == pos2 : 
-					pos2 = random.randint(0,dupBestPop[0].N-1)
-
-				#print "Valeurs de pos1/pos2 \n",pos1,pos2
-
-				p1 = min(pos1,pos2)
-				p2 = max(pos1,pos2)
-
-				# Mise en place du croisement:
-				graphe1 = dupBestPop[g1].graphe
-				graphe2 = dupBestPop[g2].graphe
-				temp1 = np.copy(graphe1)
-				temp2 = np.copy(graphe2)
-
-				# print "Les deux graphes a croises sont \n"
-				# print "g1" , graphe1
-				# print ""
-				# print "g2" ,graphe2
-
-
-				
-				for i in xrange(p1,p2+1):
-					
-					#On modifie le premier graphe
-				   
-					graphe1[l1,i] = temp2[l1,i]
-					graphe1[i,l1] = temp2[i,l1]
-
-					#Puis le deuxieme graphe
-
-					graphe2[l1,i] = temp1[l1,i]
-					graphe2[i,l1] = temp1[i,l1]
-
-				# Puis on remplace par les nouveaux graphes dans la population que l'on croise.    
-				dupBestPop[g1].graphe = graphe1
-				dupBestPop[g2].graphe = graphe2
-
-
-				# print "Les deux graphes a croises sont \n"
-				# print "g1" , graphe1
-				# print ""
-				# print "g2" ,graphe2
-
-			#La nouvelle population contient les anciens meilleurs, puis les nouveaux 
-			# Qui sont remanier 
-			#newPop = []
-			#newPop.append(bestPop)
-			#newPop.append(dupBestPop)
-			return dupBestPop
-			
-        def saveInFile(self,nameFile="info.txt"):
-			f=open(nameFile,"w")
-			f.write("taillePop: %f\n"%self.p)
-			f.write("tailleGraphe: %f\n"%self.tailleGraphe)
-			f.write("Proba: %f\n"%self.proba)
-			f.close()
-
-
-####################################################################################
-##                                                                                ## 
-##                            CLASS SIMULATION                                    ##
-##                                                                                ## 
-####################################################################################
-
-
-class Simulation:
-	def __init__(self,pop,seuil,nbCrois):
-		self.pop=pop
-		self.seuil=seuil
-		self.nbCrois=nbCrois
-
-	
-	def coutMin_Max(self):
-		couts=[]
-		for i in xrange(len(self.pop.population)):
-			couts.append(self.pop.population[i].cout)
-		return min(couts),max(couts)
-		
-		
-	def generation(self):
-		compt=0 #compteur
-		while compt < self.seuil:
-			#SELECTION
-
-			popSelect=self.pop.selection()
-
-			#CROISEMENT
-			self.pop.croisement(popSelect,self.nbCrois)
-			
-			#MUTATION
-			for i in xrange(len(popSelect)):
-				self.pop.mutation(popSelect[i].graphe)
-			
-			#MAJ DE LA POP
-			for k in xrange(self.pop.p):
-				self.pop.population[k]=popSelect[k]
-				
-			compt+=1
-			#print "nombre de tour",compt
-
-	
-			
-
-
-
-
-popu = Population(50,0.9,50)
-
-
-
-
-
-simul=Simulation(popu,50,5)
-simul.generation()
-
-g = simul.pop.population[0]
-#print g
-
-nodes = g.nodesAndEdges()[0]
-edges = g.nodesAndEdges()[1]
-#print ("Edge",edges)
-#print ("Nodes",nodes)
-=======
 g = simul.pop.population[0]
 drawSCE(g)
 
 nodes = g.nodesAndEdges()[0]
 edges = g.nodesAndEdges()[1]
->>>>>>> refs/remotes/origin/master
 
 plt.figure("figure 2")
 
 G=nx.Graph()  
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
-<<<<<<< HEAD
-#print G.number_of_nodes()
-#print G.number_of_edges()
-nx.draw(G,node_color="pink") # ROSE bien evidemment ;)
-plt.show()
-
-#print g.sceDegrees()[1]
-
-
-
-it = range(1,51)
-
-#print len(it)
-distributionTheo = list(g.sceDegrees()[2])
-distributionTheo = distributionTheo[0:50]
-print len(distributionTheo)
-print len(g.sceDegrees()[1])
-
-
-plt.plot(it,g.sceDegrees()[1],marker='o',color='cyan')
-plt.plot(it,distributionTheo,marker='v',color='purple')
-plt.title("Distribution theorique de la somme des carres des ecarts / Distibution observee")
-#plt.legend([p1,p2],["Theorique","Observee"])
-plt.show()
-
-
-
-
-
-=======
 print (G.number_of_nodes())
 print (G.number_of_edges())
 nx.draw(G,node_color="pink") # ROSE bien evidemment ;)
 plt.show()
->>>>>>> refs/remotes/origin/master
 
 """
 
@@ -449,20 +166,15 @@ temp=np.copy(temp2)
                 
 print d
 print temp
-'''
 
 #erreur qui survient de temps en temps
-'''
+
 Traceback (most recent call last):
   File "aggpGraphe.py", line 411, in <module>
     popu.croisement()
   File "aggpGraphe.py", line 336, in croisement
     self.mutation(dupBestPop[l].graphe)
 AttributeError: 'int' object has no attribute 'graphe'
-<<<<<<< HEAD
-'''
-=======
 
 
 """
->>>>>>> refs/remotes/origin/master
